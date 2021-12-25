7b1fe296f60c2c1b4139444b3b0dab73ebf07c32
Mhshohag2256 <59985721+Mhshohag2256@users.noreply.github.com>
2021-12-24 23:42:23 -0500

    Create server and ping server endpoint impleneted

--- src/main/java/io/getarrays/server/service/ServerService.java
+++ src/main/java/io/getarrays/server/service/ServerService.java
@@ -2,11 +2,12 @@ package io.getarrays.server.service;
 
 import io.getarrays.server.model.Server;
 
+import java.net.UnknownHostException;
 import java.util.Collection;
 
 public interface ServerService {
     Server create(Server server);
-    Server ping(String ipAddress);
+    Server ping(String ipAddress) throws UnknownHostException;
     Collection<Server> list(int limit);
     Server get(Long id);
     Server update(Server server);
--- src/main/java/io/getarrays/server/service/implementation/ServerServiceImplementation.java
+++ src/main/java/io/getarrays/server/service/implementation/ServerServiceImplementation.java
@@ -1,19 +1,45 @@
 package io.getarrays.server.service.implementation;
 
+import io.getarrays.server.enumeration.Status;
 import io.getarrays.server.model.Server;
+import io.getarrays.server.repo.ServerRepo;
 import io.getarrays.server.service.ServerService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.Collection;
 
+import static io.getarrays.server.enumeration.Status.SERVER_DOWN;
+import static io.getarrays.server.enumeration.Status.SERVER_UP;
+
+@RequiredArgsConstructor
+@Service
+@Transactional
+@Slf4j
 public class ServerServiceImplementation implements ServerService {
-    @Override
-    public Server create(Server server) {
-        return null;
-    }
+    private final ServerRepo serverRepo;
 
     @Override
-    public Server ping(String ipAddress) {
-        return null;
+    public Server create(Server server) {
+        log.info("Saving new server: {}",server.getName());
+        server.setImageUrl(setServerImageUrl());
+        return serverRepo.save(server);
+    }
+
+
+    @Override
+    public Server ping(String ipAddress) throws IOException {
+        log.info("Pinging server IP: {}",ipAddress);
+        Server server = serverRepo.findByIpAddress(ipAddress);
+        InetAddress address = InetAddress.getByName(ipAddress);
+        server.setStatus(address.isReachable(10000)? SERVER_UP : SERVER_DOWN);
+        serverRepo.save(server);
+        return server;
     }
 
     @Override
@@ -33,6 +59,10 @@ public class ServerServiceImplementation implements ServerService {
 
     @Override
     public Boolean delete(Long id) {
+        return null;
+    }
+
+    private String setServerImageUrl() {
         return null;
     }
 }
